2fdfefc8a6d68c2fb72c160b53ddb12817713561
dddd <abbeyokgo@gmail.com>
2026-06-27 21:47:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  "docker.tkbond.net": dockerHub,
+  "quay.tkbond.net": "https://quay.io",
+  "gcr.tkbond.net": "https://gcr.io",
+  "k8s-gcr.tkbond.net": "https://k8s.gcr.io",
+  "k8s.tkbond.net": "https://registry.k8s.io",
+  "ghcr.tkbond.net": "https://ghcr.io",
+  "cloudsmith.tkbond.net": "https://docker.cloudsmith.io",
+  "ecr.tkbond.net": "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  "docker-staging.tkbond.net": dockerHub,
 };
 
 function routeByHosts(host) {
